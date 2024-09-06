7ace91face7b4b3a2ca51a9289902ab879f074ad
Uththara Samadhi <uththarasamadhi66@gmail.com>
2024-09-06 11:34:12 +0530

    priority test example

--- src/test/java/testNG/PriorityTest.java
+++ src/test/java/testNG/PriorityTest.java
@@ -4,27 +4,22 @@ import org.testng.annotations.Test;
 
 public class PriorityTest {
 
-//    @Test(priority = 0)
-//    public void testLogin(){
-//        System.out.println("login");
-//    }
+    @Test(priority = 0)
+    public void TestLogin(){
+        System.out.println("Login");
+    }
     @Test(priority = 1)
-    public void testCreate(){
-        System.out.println("create");
+    public void TestCreate(){
+        System.out.println("Create");
 
-
-    }
-     @Test(priority = 2,enabled = false)
-    public void testEdit(){
-        System.out.println("Edit");
-    }
-    @Test(priority = 4)
-    public void testDelete(){
-        System.out.println("delete");
     }
     @Test(priority = 3)
-    public void testLogout(){
-        System.out.println("logout");
+    public void TestDelete(){
+        System.out.println("Delete");
+    }
+    @Test(priority = 2)
+    public void TestLogout(){
+        System.out.println("Logout");
     }
 
 
