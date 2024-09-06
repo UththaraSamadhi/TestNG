2c1be9bf618ffae52d18b2a100b48810e75a465c
Uththara Samadhi <uththarasamadhi66@gmail.com>
2024-09-06 09:21:39 +0530

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
+package org.example;
+
+
 public class Main {
-    public static void main(String[]args){
-        System.out.println("The test 1");
+    public static void main(String[] args) {
+        System.out.println("Initial test");
+
     }
 }
